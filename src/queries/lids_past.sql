53c046b6df8bb1c9cc459e43e3f1e64bd147c115
Srijan Deo <srdeo@MB-K096NXK4T1.local>
2026-06-12 01:45:02 +0530

    edit4

--- src/queries/lids_past.sql
+++ src/queries/lids_past.sql
@@ -57,9 +57,37 @@ FROM `cprtpr-dataplatform-sp1`.usviews.v_us_lot_image_damage_scores_fact a1
     join (
     SELECT
         l.lot_nbr FROM `cprtpr-dataplatform-sp1`.usviews.v_us_lot_fact l
-                join `cprtpr-dataplatform-sp1.usviews.v_us_bids_fact` b
-                on l.lot_nbr = b.lot_nbr
-where b.auc_dt = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
-        AND lot_type_cd IN ('V')
-        AND yard_country_cd = 'USA') lots
-    on a1.lot = lots.lot_nbr
+                where lot_nbr in (with ranked as (
+  select b.lot_nbr,
+         l.lot_year,
+         l.lot_make_cd,
+         l.grp_model,
+         l.acv,
+         l.plug_lot_acv,
+         l.repair_cost,
+         l.yard_state_cd,
+         l.sales_title_grp_desc,
+         b.buyer_nbr,
+         mbr_lic_type,
+         f.mbr_state,
+         bid_dttm, row_number() over (partition by b.lot_nbr order by b.bid_dttm desc) as rnk
+  from `cprtpr-dataplatform-sp1.usviews.v_us_bids_fact` b
+  join `cprtpr-dataplatform-sp1.usviews.v_us_lot_fact` l
+    on b.lot_nbr = l.lot_nbr
+    join `cprtpr-dataplatform-sp1`.usviews.v_us_member_fact f
+    on b.buyer_nbr = f.mbr_nbr
+    where mbr_country = 'USA'
+            AND mbr_mbrshp_type_cd IN ('BASIC', 'PREMIER')
+            AND mbr_site_status_cd = 'A'
+            AND mbr_mbrshp_status_cd = 'A'
+            AND mbr_lang_pref = 'en'
+            AND mbr_status = 'A'
+AND b.auc_dt = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+and lot_type_cd = 'V'
+            and yard_country_cd = 'USA'
+            and mbr_access_type not in ('BROKER', 'BRKBDR', 'MSTRBDR', 'MASTER')
+)
+select lot_nbr
+from ranked
+where rnk = 2
+)
